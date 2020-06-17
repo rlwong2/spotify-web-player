699556d42f3d1187c91af3b2c49fc30ea70abc2a
Riley Wong <rlwong2@gmail.com>
2020-06-17 17:32:51 -0500

    Shows Now Playing

---
+++ src/config_example.js
@@ -0,0 +1,9 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+export const clientId = "YOUR_CLIENT_ID_HERE";
+export const redirectUri = "http://localhost:3000/redirect";
+export const scopes = [
+    "user-top-read",
+    "user-read-currently-playing",
+    "user-read-playback-state",
+];
